4096b4735aa24b0033bc06c49d7748630dcca552
Jadon Naas <30273534+jadonn@users.noreply.github.com>
2022-04-10 22:10:30 -0400

    Basic proof of concept reading text from PDF

--- src/components/Keywords.svelte
+++ src/components/Keywords.svelte
@@ -4,6 +4,26 @@
 function processFiles() {
     const fileList = pdfUpload.files;
     console.log(fileList);
+    for (let index = 0; index < fileList.length; index++) {
+        const currentFile = fileList[index];
+        const reader = new FileReader();
+        reader.addEventListener("load", async function() {
+            const loadingTask = pdfjsLib.getDocument(reader.result);
+            const pdf = await loadingTask.promise;
+            const numPages = pdf.numPages;
+            for (let pageIndex = 1; pageIndex < numPages; pageIndex++) {
+                const page = await pdf.getPage(pageIndex);
+                const textContent = await page.getTextContent();
+                console.log(textContent);
+                textContent.items.forEach(function(textItem) {
+                    console.log(textItem.str);
+                })
+            }
+        });
+
+        reader.readAsDataURL(currentFile);
+        
+    }
 }
 
 </script>
