f1943b88c99da86232d70fefd0d4c540be5f7e3d
Jadon Naas <30273534+jadonn@users.noreply.github.com>
2022-04-10 20:13:41 -0400

    Very basic file upload element

---
+++ src/components/Keywords.svelte
@@ -0,0 +1,10 @@
+<script>
+    let pdfUpload;
+
+function handleFiles() {
+    const fileList = pdfUpload.files;
+    console.log(fileList);
+}
+
+</script>
+<input type="file" id="pdfUpload" on:change={handleFiles} bind:this={pdfUpload} multiple />
