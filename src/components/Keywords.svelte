5f3bd8ae014f3a004c6eaf24b43cb08002d2a72c
Jadon Naas <30273534+jadonn@users.noreply.github.com>
2022-04-12 20:10:10 -0400

    Working proof of concept of keyword matching

--- src/components/Keywords.svelte
+++ src/components/Keywords.svelte
@@ -1,5 +1,7 @@
 <script>
+    let searchTerm;
     let pdfUpload;
+    const matchingFiles = [];
 
 function processFiles() {
     const fileList = pdfUpload.files;
@@ -11,21 +13,39 @@ function processFiles() {
             const loadingTask = pdfjsLib.getDocument(reader.result);
             const pdf = await loadingTask.promise;
             const numPages = pdf.numPages;
+            const matches = [];
             for (let pageIndex = 1; pageIndex < numPages; pageIndex++) {
                 const page = await pdf.getPage(pageIndex);
                 const textContent = await page.getTextContent();
-                console.log(textContent);
                 textContent.items.forEach(function(textItem) {
-                    console.log(textItem.str);
+                    const text = textItem.str;
+                    if(searchTerm !== undefined && searchTerm !== "" && text !== undefined && text.includes(searchTerm)){
+                        matches.push({
+                            pageNumber: pageIndex,
+                            text: text
+                        });
+                    }
                 })
             }
+            if(matches.length !== 0){
+                matchingFiles.push({
+                    fileName: currentFile.name, 
+                    matches: matches
+                });
+                matchingFiles = matchingFiles;
+            };
         });
-
         reader.readAsDataURL(currentFile);
         
     }
 }
 
 </script>
+<label for="searchTerm">Search Term:</label>
+<input type="text" id="searchTerm" bind:value={searchTerm} />
 <input type="file" id="pdfUpload" bind:this={pdfUpload} multiple />
 <button on:click={processFiles}>Process Files</button>
+
+{#each matchingFiles as match}
+<p>{match.fileName}</p>
+{/each}
