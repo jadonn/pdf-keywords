453dd71969889e1a5b96cda9ebca355a45b2ee02
Jadon Naas <30273534+jadonn@users.noreply.github.com>
2022-04-13 20:15:13 -0400

    Not working implementation of workers with PDF JS

---
+++ public/worker.js
@@ -0,0 +1,30 @@
+self.importScripts("pdf.min.js");
+self.importScripts("pdf.worker.min.js");
+self.importScripts("pdf.worker.entry.min.js");
+pdfjsLib.GlobalWorkerOptions.workerSrc = "pdf.worker.min.js";
+onmessage = async function (e){
+    const data = e.data;
+    console.log(data);
+    const currentFile = data.currentFile;
+    const searchTerm = data.searchTerm;
+    const reader = new FileReaderSync();
+    const result = reader.readAsDataURL(currentFile);
+    const loadingTask = pdfjsLib.getDocument(result);
+    const pdf = await loadingTask.promise;
+    const numPages = pdf.numPages;
+    const matches = [];
+    for (let pageIndex = 1; pageIndex < numPages; pageIndex++) {
+        const page = await pdf.getPage(pageIndex);
+        const textContent = await page.getTextContent();
+        textContent.items.forEach(function(textItem) {
+            const text = textItem.str;
+            if(searchTerm !== undefined && searchTerm !== "" && text !== undefined && text.includes(searchTerm)){
+                matches.push({
+                    pageNumber: pageIndex,
+                    text: text
+                });
+            }
+        })
+    }
+    postMessage(matches);
+}
--- src/components/Keywords.svelte
+++ src/components/Keywords.svelte
@@ -1,7 +1,38 @@
 <script>
     let searchTerm;
     let pdfUpload;
-    const matchingFiles = [];
+    const workerLimit = 3;
+    let matchingFiles = [];
+
+function processFilesWithWorkers() {
+    const fileList = pdfUpload.files;
+    let processedCount = 0;
+    const processedFiles = [];
+    const fileMatches = [];
+    for(let index = 0; index < workerLimit; index++){
+        const newWorker = new Worker("worker.js");
+        newWorker.onmessage = function(e) {
+            const data = e.data;
+            processedFiles.push(data);
+            const matches = data.matches;
+            if(matches !== undefined && matches.length !== 0) {
+                fileMatches.push(data);
+            }
+            if(fileList.length === processedCount.length) {
+                newWorker.terminate();
+                console.log("Terminating worker");
+            }else {
+                newWorker.postMessage({currentFile: fileList[processedCount], searchTerm: searchTerm});
+                console.log(fileMatches);
+                processedCount += 1;
+            }
+            
+        }
+        newWorker.postMessage({currentFile: fileList[index], searchTerm: searchTerm});
+        processedCount += 1;
+    }
+}
+
 
 function processFiles() {
     const fileList = pdfUpload.files;
@@ -44,7 +75,7 @@ function processFiles() {
 <label for="searchTerm">Search Term:</label>
 <input type="text" id="searchTerm" bind:value={searchTerm} />
 <input type="file" id="pdfUpload" bind:this={pdfUpload} multiple />
-<button on:click={processFiles}>Process Files</button>
+<button on:click={processFilesWithWorkers}>Process Files</button>
 
 {#each matchingFiles as match}
 <p>{match.fileName}</p>
